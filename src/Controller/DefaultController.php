a884fb9e59b59b7ad945feb279500f9ec6ddc710
Pierre Bretéché <pbreteche@dawan.fr>
2024-05-27 17:28:58 +0200

    Symfony Cache contract

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,8 +4,11 @@ namespace App\Controller;
 
 use App\Entity\Purchase;
 use App\Event\PurchaseConfirmedEvent;
+use Psr\Cache\CacheItemInterface;
+use Psr\Cache\CacheItemPoolInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\Cache;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
 
@@ -20,5 +23,19 @@ class DefaultController extends AbstractController
         $eventDispatcher->dispatch(new PurchaseConfirmedEvent($purchase));
 
         return $this->render('default/homepage.html.twig');
+    }
+
+    #[Route('/cache-demo')]
+    public function stats(
+        CacheItemPoolInterface $pool,
+    ): Response {
+        $stats = $pool->get('app_default_stats', function (CacheItemInterface $item) {
+            $item->expiresAfter(new \DateInterval('PT1H'));
+            sleep(10); // Emulate a heavy process
+
+            return [1, 2, 3];
+        });
+
+        return $this->json($stats);
     }
 }
