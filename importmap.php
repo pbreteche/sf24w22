ada17ca603d01121cd33d3c34f2d38503fee3cfa
Pierre Bretéché <pbreteche@dawan.fr>
2024-05-30 15:04:57 +0200

    Bootstrap sass

--- importmap.php
+++ importmap.php
@@ -31,8 +31,4 @@ return [
     '@popperjs/core' => [
         'version' => '2.11.8',
     ],
-    'bootstrap/dist/css/bootstrap.min.css' => [
-        'version' => '5.3.3',
-        'type' => 'css',
-    ],
 ];
