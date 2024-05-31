3ea99dcf76c6ee31369adc0f95d8e3dd9a2dba85
Pierre Bretéché <pbreteche@dawan.fr>
2024-05-31 11:09:02 +0200

    Data provider

--- tests/Service/CalendarTest.php
+++ tests/Service/CalendarTest.php
@@ -3,15 +3,25 @@
 namespace App\Tests\Service;
 
 use App\Service\Calendar;
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
 
 class CalendarTest extends TestCase
 {
-    public function testIsWeekend()
+    #[DataProvider('isWeekendProvider')]
+    public function testIsWeekend($input1, $expected)
     {
         $calendar = new Calendar();
-        $result = $calendar->isWeekend(new \DateTimeImmutable('2024-05-31'));
+        $result = $calendar->isWeekend(new \DateTimeImmutable($input1));
 
-        $this->assertFalse($result, '2024-05-31 is friday, not weekend');
+        $this->assertEquals($expected, $result, '2024-05-31 is friday, not weekend');
+    }
+
+    public static function isWeekendProvider(): array
+    {
+        return [
+            ['2024-05-31', false],
+            ['2024-05-25', true],
+        ];
     }
 }
